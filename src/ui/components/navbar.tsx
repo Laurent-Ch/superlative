9efd24e61d02ff614b1835a97e2efd8b9de96777
Laurent-Ch <chamouleau.laurent@gmail.com>
2025-03-12 23:10:21 +0100

    Draft navbar left-side logo

--- src/ui/components/navbar.tsx
+++ src/ui/components/navbar.tsx
@@ -15,63 +15,74 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <div className={`w-2/5 text-xl flex flex-col gap-2 ${styles["navbar"]}`}>
-      <div className="w-full flex gap-2 justify-between">
-        {links.map(({ href, label }) => (
-          <Link
-            key={href}
-            href={href}
-            className={`flex-1 flex gap-1 justify-center items-center ${
-              styles.link
-            } ${pathname === href ? styles.active : ""}`}
-          >
-            <span className="w-[5px]" aria-hidden="true" />
-            {label}
-            {pathname === href ? (
-              <Image
-                className={`self-start ${pathname === href ? "" : "invisible"}`}
-                src="/logoPink.svg"
-                alt=""
-                width={5}
-                height={5}
-                sizes="90vw"
-              />
-            ) : (
-              <span className="w-[5px]" aria-hidden="true" />
-            )}
-          </Link>
-        ))}
+    <div className="navbar__container">
+      <div
+        className={`fixed h-[4rem] top-[2rem] left-[2rem] flex justify-center items-center`}
+      >
+        <Image src="/logo.svg" alt="" width={23} height={23} sizes="90vw" />
       </div>
-      {pathname === "/manifesto" && (
-        <>
-          <div className="relative flex w-full overflow-x-hidden text-white">
-            <div className="animate-marquee whitespace-nowrap">
-              <span>
-                A passionate and assertive curation of all copywriting that exudes
-                cachet.&nbsp;
-              </span>
+      <div
+        className={`w-2/5 min-h-[4rem] text-xl flex flex-col gap-2 ${styles["navbar"]}`}
+      >
+        <div className="w-full flex gap-2 justify-between">
+          {links.map(({ href, label }) => (
+            <Link
+              key={href}
+              href={href}
+              className={`flex-1 flex gap-1 justify-center items-center ${
+                styles.link
+              } ${pathname === href ? styles.active : ""}`}
+            >
+              <span className="w-[5px]" aria-hidden="true" />
+              {label}
+              {pathname === href ? (
+                <Image
+                  className={`self-start ${
+                    pathname === href ? "" : "invisible"
+                  }`}
+                  src="/logoPink.svg"
+                  alt=""
+                  width={5}
+                  height={5}
+                  sizes="90vw"
+                />
+              ) : (
+                <span className="w-[5px]" aria-hidden="true" />
+              )}
+            </Link>
+          ))}
+        </div>
+        {pathname === "/manifesto" && (
+          <>
+            <div className="relative flex w-full overflow-x-hidden text-white">
+              <div className="animate-marquee whitespace-nowrap">
+                <span>
+                  A passionate and assertive curation of all copywriting that
+                  exudes cachet.&nbsp;
+                </span>
+              </div>
+              <div className="absolute top-0 inline-block animate-marquee2 whitespace-nowrap">
+                <span>
+                  A passionate and assertive curation of all copywriting that
+                  exudes cachet.&nbsp;
+                </span>
+              </div>
             </div>
-            <div className="absolute top-0 inline-block animate-marquee2 whitespace-nowrap">
-              <span>
-                A passionate and assertive curation of all copywriting that exudes
-                cachet.&nbsp;
-              </span>
-            </div>
-          </div>
 
-          <div className="w-full flex gap-2 justify-between text-white">
-            <div className="flex-1 flex justify-center items-center">
-              Visions
+            <div className="w-full flex gap-2 justify-between text-white">
+              <div className="flex-1 flex justify-center items-center">
+                Vision
+              </div>
+              <div className="flex-1 flex justify-center items-center">
+                Mission
+              </div>
+              <div className="flex-1 flex justify-center items-center">
+                Valeurs
+              </div>
             </div>
-            <div className="flex-1 flex justify-center items-center">
-              Mission
-            </div>
-            <div className="flex-1 flex justify-center items-center">
-              Valeurs
-            </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
